2ac0a80897596d706d2b5b409432e6d4a15dc602
Fernando Cordeiro <fernando_cordeiro@live.com>
2023-03-11 20:17:26 +0000

    🐛 FIX: Missing comma

--- gateway/gateway/auth.py
+++ gateway/gateway/auth.py
@@ -1,20 +1,24 @@
 import os
 
+from typing import Optional
+
 import requests
 
 
-def login(request: requests.Request) -> tuple[str, tuple[str, int]]:
+Token = str
+ResponseError = tuple[str, int]
+
+
+def login(request: requests.Request) -> tuple[Token | None, Optional[ResponseError]]:
     """Forward login request to auth service"""
     auth = request.authorization
     if not auth:
-        return None("Missing credentials", 401)
+        return None, ("Missing credentials", 401)
 
     basicAuth = (auth.username, auth.password)
-
     response = requests.post(
         f"http://{os.environ.get('AUTH_SVC_ADDRESS')}/login", auth=basicAuth
     )
-
     if response.status_code == 200:
         return response.text, None
     else:
